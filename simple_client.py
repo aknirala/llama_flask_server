73434884e37dba30cc96204ab4f169b12fa94889
Ashutosh Kumar Nirala <akn.nirala@gmail.com>
2025-01-28 17:27:00 -0600

    added client as well

---
+++ simple_client.py
@@ -0,0 +1,39 @@
+import requests
+import base64
+
+url = "http://10.24.47.116:5000/generate"
+
+# Example message (one dialog)
+messages = [
+    {"role": "user", "content": "I am going to Paris, what should I see?"},
+#    {"role": "assistant", "content": "Visit the Eiffel Tower and the Louvre Museum."},
+#    {"role": "user", "content": "What is so great about #1?"}
+]
+
+data = {"messages": messages}
+
+response = requests.post(url, json=data)
+
+print(response.json())  # Print the response from the server
+
+
+# Load and encode image
+with open("models/scripts/resources/dog.jpg", "rb") as img_file:
+    img_base64 = base64.b64encode(img_file.read()).decode("utf-8")
+
+# Example messages (one dialog)
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "data": img_base64},
+            {"type": "text", "text": "Describe this image in two sentences."}
+        ]
+    }
+]
+
+data = {"messages": messages}
+
+response = requests.post(url, json=data)
+
+print(response.json())  # Print the response from the server
