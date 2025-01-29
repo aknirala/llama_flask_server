00516917710117bfc0e595db0b31c4ea637d2866
Ashutosh Kumar Nirala <akn.nirala@gmail.com>
2025-01-29 07:59:04 -0600

    fixed typo in run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 set -x
 CHECKPOINT_DIR=~/.llama/checkpoints/Llama3.2-1B-Instruct
-torchrun llama_models/scripts/example_chat_completion.py $CHECKPOINT_DIR
+torchrun multimedia_server.py $CHECKPOINT_DIR
 set +x
